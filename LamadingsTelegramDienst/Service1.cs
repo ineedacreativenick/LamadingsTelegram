7344f512cfdb011e5c7c138988ec95e9c1337c42
compymaccompface <myfourchanemailadress@gmail.com>
2016-09-06 22:49:05 +0200

    initial

--- LamadingsTelegramDienst/Service1.cs
+++ LamadingsTelegramDienst/Service1.cs
@@ -1,86 +1,93 @@
 ﻿using Meebey.SmartIrc4net;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 
-namespace LamadingsTelegram
+namespace LamadingsTelegramDienst
 {
-    class Program
-    {
 
+
+
+
+    public partial class Service1 : ServiceBase
+    {
         private static readonly TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient(ConfigurationManager.AppSettings["TelegramBotId"]);
         public static IrcClient irc = new IrcClient();
         public static string IrcChan = ConfigurationManager.AppSettings["IrcChannel"];
 
-        static void Main(string[] args)
+
+        public Service1()
         {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
 
             #region telegram
             Bot.OnMessage += BotOnMessageReceived;
+
             var me = Bot.GetMeAsync().Result;
+
+            
             Bot.StartReceiving();
+
             #endregion
 
-            #region irc
             irc.SendDelay = 200;
             irc.AutoRetry = true;
             irc.ActiveChannelSyncing = true;
             irc.OnRawMessage += new IrcEventHandler(OnRawMessage);
-            irc.AutoReconnect = true;
+
             string[] serverlist;
             serverlist = new string[] { ConfigurationManager.AppSettings["IrcServer"] };
 
             int port = 6667;
             irc.Connect(serverlist, port);
-
-            irc.UseSsl = true;
+            //irc.UseSsl = true;
             irc.EnableUTF8Recode = true;
             irc.Encoding = System.Text.Encoding.UTF8;
 
-            irc.Login("Lamadingbot", "Stupid Bot");
+            irc.Login("Lamabot", "Stupid Bot");
             irc.RfcJoin(IrcChan);
-
             irc.SendMessage(SendType.Message, IrcChan, "#makeircgreatagain");
 
             irc.Listen();
-            #endregion
 
-
-
-            Console.ReadLine();
-            irc.Disconnect();
-            Bot.StopReceiving();
         }
 
-        /// <summary>
-        /// event that fires when irc bot receives a message
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        protected override void OnStop()
+        {
+            Bot.StopReceiving();
+            irc.Disconnect();
+        }
+
+
         private static void OnRawMessage(object sender, IrcEventArgs e)
         {
-            var x = Bot.GetMeAsync().Result;
-            //only chan messages from users 
             if (!string.IsNullOrEmpty(e.Data.Message) && !string.IsNullOrEmpty(e.Data.Nick) && e.Data.Type == ReceiveType.ChannelMessage)
             {
-                
-                var t = Bot.SendTextMessageAsync(ConfigurationManager.AppSettings["TelegramChanId"], e.Data.Nick + "(irc): " + e.Data.Message);
+
+                var t = Bot.SendTextMessageAsync(ConfigurationManager.AppSettings["TelegramBotId"], e.Data.Nick + "(irc): " + e.Data.Message);
             }
         }
 
-
-        /// <summary>
-        /// event that fires when telegram bot receives a message
-        /// pastes text into irc 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="messageEventArgs"></param>
-        private static void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        private static  void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
 
             var message = messageEventArgs.Message.Text;
             irc.SendMessage(SendType.Message, IrcChan, messageEventArgs.Message.From.Username + " (telegram): " + message);
+            //var t = await Bot.SendTextMessageAsync("-176245781", "nachricht erhalten: " + messageEventArgs.Message.From.Username + message);
         }
     }
 }
